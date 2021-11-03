8515dd5fedc830ce915149124931cbf7196bfb5b
Arafathislam <islamarafath315@gmail.com>
2021-11-03 12:53:20 +0600

    A. Young Physicist

---
+++ 69A.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	 int n,x,y,z,sum1=0,sum2=0,sum3=0;
+	cin>>n;
+	for(int i=0;i<n;i++){
+		cin>>x>>y>>z;
+		sum1+=x;
+		sum2+=y;
+		sum3+=z;
+	}
+	if(sum1==0 && sum2==0 && sum3==0){
+		cout<<"YES"<<endl;
+	}else{
+		cout<<"NO"<<endl;
+	}
+	return 0;
+}
